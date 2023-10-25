68797959bca0ca598c13c3c8525e563c35822cd5
Phạm Quốc Huy <phamquochuy302002@gmail.com>
2023-10-25 22:44:43 +0700

    branch dev

--- lib/mirrorscripts.ts
+++ lib/mirrorscripts.ts
@@ -1,6 +1,6 @@
 export const getHostName = () => {
-    const hostName = 'mirror-scripts-v2-production.up.railway.app';
-    // const hostName = 'mirror-scripts-v2-dev.up.railway.app';
+    // const hostName = 'mirror-scripts-v2-production.up.railway.app';
+    const hostName = 'mirror-scripts-v2-dev.up.railway.app';
     // const hostName = 'localhost:8000';
     return hostName;
 }
